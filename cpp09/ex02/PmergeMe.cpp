d65f99ab4a5d9737d5086d7bcc2396d5d03a86b1
Ethan <ethanberko@gmail.com>
2025-08-20 14:11:18 +0300

    latest

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: eberkowi <eberkowi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 11:51:58 by eberkowi          #+#    #+#             */
-/*   Updated: 2025/08/20 12:21:53 by eberkowi         ###   ########.fr       */
+/*   Updated: 2025/08/20 14:11:08 by eberkowi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void handleInput(char **argv, std::vector<unsigned int> &input) {
 	for (int i = 1; argv[i]; i++) {
-		//std::cout << argv[i] << std::endl;
 		unsigned long temp;
 		try {
 			temp = std::stoul(argv[i]);
@@ -29,7 +28,40 @@ void handleInput(char **argv, std::vector<unsigned int> &input) {
 	}	
 }
 
+
+void addInputToNodes(std::vector<unsigned int> &input, std::vector<node> &nodes) {
+	for (unsigned int n : input) {
+		node temp;
+		temp.largest = n;
+		nodes.push_back(temp);
+	}
+}
+
+std::vector<node> handlePairComparisons(std::vector<node> nodes) {
+	unsigned int vector_size = nodes.size();
+	std::vector<node> result;
+	
+	unsigned int i = 0;
+	for (;i + 1 < vector_size; i += 2) {
+		std::cout << "first = " << nodes[i].largest << " second = " << nodes[i + 1].largest << std::endl;
+		if (nodes[i].largest > nodes[i + 1].largest) {
+			node temp;
+			temp.largest = nodes[i].largest;
+			//temp.other.push_back
+		}
+	}
+	if (vector_size % 2 == 1) {
+		std::cout << "leftover = " << nodes[i].largest  << std::endl;
+	}
+}
+
 void PmergeMe(char **argv) {
 	std::vector<unsigned int> input;
 	handleInput(argv, input);
+
+	std::vector<node> nodes;
+	addInputToNodes(input, nodes);
+
+	std::vector<node> new_nodes;
+	new_nodes = handlePairComparisons(nodes);
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,12 +6,19 @@
 /*   By: eberkowi <eberkowi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 11:51:58 by eberkowi          #+#    #+#             */
-/*   Updated: 2025/08/20 12:09:01 by eberkowi         ###   ########.fr       */
+/*   Updated: 2025/08/20 13:40:32 by eberkowi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <vector>
 #include <limits>
+
+struct node {
+    unsigned int largest;
+	std::vector<unsigned int> other;
+	unsigned int level = 0;
+	std::string type = "pair";
+};
 
 void PmergeMe(char **argv);
